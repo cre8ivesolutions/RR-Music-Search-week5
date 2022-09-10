92437a0797476466241b22baaa84d89060a25a48
cre8ivesolutions <tammerz78@gmail.com>
2022-09-09 20:56:18 -0500

    final commit week 5

---
+++ src/components/ArtistView.js
@@ -0,0 +1,49 @@
+import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { useParams, Link } from 'react-router-dom'
+
+function ArtistView() {
+    const { id } = useParams()
+    const [ artistData, setArtistData ] = useState([])
+    const navigate = useNavigate()
+    const navButtons = () => {
+        return(
+            <div>
+                <button onClick={() => navigate(-1)}>Back</button>
+                <button onClick={() => navigate('/')}>Home</button>
+            </div>
+        )
+    }
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/album/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setArtistData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    const justAlbums = artistData.filter(entry => entry.collectionType === 'Album')
+
+    const renderAlbums = justAlbums.map((album, i) => {
+        return (
+            <div key={i}>
+                <Link to={`/album/${album.collectionId}`}>
+                    <p>{album.collectionName}</p>
+                </Link>
+            </div>
+            )
+        })
+
+    return (
+      <div className="App">
+            {navButtons()}
+            <h2>The id passed was: {id}</h2>
+            <h1>Artist: {artistData} </h1>
+            {renderAlbums}
+        </div>
+    )
+}
+
+export default ArtistView
